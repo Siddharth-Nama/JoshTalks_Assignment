4d7203aed4939ae37e8361f5c6a75870f1929ce3
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-03-25 01:48:41 +0530

    Assignment completed

--- JoshTalks_Assignment/JoshTalks_Assignment/urls.py
+++ JoshTalks_Assignment/JoshTalks_Assignment/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from Taskmanagement.models import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),
+    path('api/', include('Taskmanagement.urls')),
 ]
