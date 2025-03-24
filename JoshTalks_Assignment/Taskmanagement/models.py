738f8c3fa15f7310984c0f92d08630bb91280cea
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-03-25 01:21:10 +0530

    Model created

--- JoshTalks_Assignment/Taskmanagement/models.py
+++ JoshTalks_Assignment/Taskmanagement/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    task_type = models.CharField(max_length=50)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=20, default='pending')
+    assigned_users = models.ManyToManyField(User, related_name='tasks')
+
+    def __str__(self):
+        return self.name
